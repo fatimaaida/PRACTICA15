225c7b7a01391ebeae9ec23231ec3c1113c50170
fatimaaida <fatima.leon19@itca.edu.sv>
2022-05-08 11:55:40 -0600

    EJERCICIO1

---
+++ src/programaciondefensiva/Ejercicio1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programaciondefensiva;
+
+//FATIMA AIDA LEON REBOLLO
+public class Ejercicio1 {
+    
+    //metodo suma
+    public static int suma(int a, int b)
+    {
+        return a+b;
+    }
+    
+    //metodo multiplicacion
+    public static int multiplicacion(int a, int b)
+    {
+        return a*b;
+    }
+    
+    //metodo resta
+    public static int resta(int a, int b)
+    {
+        return a-b;
+    }
+    
+    //metodo diversion
+    public static int division(int a, int b)
+    {
+        return a/b;
+    }
+}
